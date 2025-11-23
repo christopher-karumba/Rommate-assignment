cb73014ff77cb515aed351d0dd52d17ffee03f7c
CHRISTOPHER KARUMBA NYAWIRA <christopherkarumba123@gmail.com>
2025-11-24 00:16:46 +0300

    Add files via upload

---
+++ Roommate.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name="Terry odhiambo";
+    int age=26;
+    string hobby="listening to music,writing and singing songs,watching latest movie series which i can say its his best hobby,especially the comedian and the action movies,he also likes doing atheletics especially in the mornings he usually wakes up early to take the morning runs";
+    string personality=" is that he is really calm,good,hardworking and very talkative at times";
+    string course ="a student currently like i am though he's older than i am,he's currently in his  sixth academic year here in JKUAT finalising he's course in architecture which he really enjoys and does do the drawings really well,currently he's not having much classes and he spends most of his time doing projects from guys outside which has earned him some amount of cash,he enjoys what is  about to be his  career for sure with no doubt";
+    string dailyLife="he wakes up early by seven mostly,studies and  he share  some chores at our room then and continues with his daily routine or attends class if they are";
+    cout<<"My roommate's name is"<<name<<"."<<endl;
+    cout<<"he is currently at"<<age<<"years old."<<endl;
+    cout<<"he is"<<course<<"."<<endl;
+    cout<<"everyday"<<dailyLife<<"."<<endl;
+    cout<<"his a good person and"<<personality<<"."<<endl;
+    cout<<"What he loves doing mostly is"<<hobby<<"."<<endl;
+    return 0;
+}
+
+
+
+ 
+
+
+
